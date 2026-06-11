f94b0f1eb93b548557ae40762ac6d7dc3b628872
Mttpla <mttpla@gmail.com>
2026-06-11 19:29:27 +0200

    build: add vergen-git2, emit VERGEN_GIT_DESCRIBE at compile time

--- build.rs
+++ build.rs
@@ -1,5 +1,15 @@
 fn main() {
-    // Set the minimum macOS deployment target for the linked binary.
     #[cfg(target_os = "macos")]
     println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET=11.0");
+
+    vergen_git2::Emitter::default()
+        .add_instructions(
+            &vergen_git2::Git2Builder::default()
+                .describe(true, true, None)
+                .build()
+                .unwrap(),
+        )
+        .unwrap()
+        .emit()
+        .unwrap();
 }
