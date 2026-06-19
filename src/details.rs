1c271a5490f9ce3a4da3ebd1e27494d12e56ca39
Mttpla <mttpla@gmail.com>
2026-06-19 08:33:01 +0200

    fix(details): remove show() function with expect(dead_code) suppression

--- src/details.rs
+++ src/details.rs
@@ -8,43 +8,6 @@ pub fn prepare_content(raw_json: Option<&str>) -> String {
     }
 }
 
-#[cfg(target_os = "macos")]
-#[expect(dead_code)]
-pub fn show(provider_name: &str, raw_json: Option<&str>) {
-    use objc2::{MainThreadMarker, MainThreadOnly};
-    use objc2_app_kit::{NSAlert, NSFont, NSScrollView, NSTextView};
-    use objc2_foundation::{NSPoint, NSRect, NSSize, NSString};
-
-    let content = prepare_content(raw_json);
-
-    let mtm = MainThreadMarker::new().expect("show() must be called on the main thread");
-
-    let frame = NSRect {
-        origin: NSPoint { x: 0.0, y: 0.0 },
-        size: NSSize { width: 600.0, height: 300.0 },
-    };
-
-    let scroll = NSScrollView::initWithFrame(NSScrollView::alloc(mtm), frame);
-    scroll.setHasVerticalScroller(true);
-    scroll.setHasHorizontalScroller(false);
-    scroll.setAutohidesScrollers(true);
-
-    let tv = NSTextView::initWithFrame(NSTextView::alloc(mtm), frame);
-    tv.setEditable(false);
-    tv.setSelectable(true);
-    let font = NSFont::monospacedSystemFontOfSize_weight(12.0, 0.0);
-    tv.setFont(Some(&font));
-    tv.setString(&NSString::from_str(&content));
-
-    scroll.setDocumentView(Some(&tv));
-
-    let alert = NSAlert::new(mtm);
-    alert.setMessageText(&NSString::from_str(&format!("Details \u{2014} {}", provider_name)));
-    alert.setAccessoryView(Some(&scroll));
-    alert.addButtonWithTitle(&NSString::from_str("OK"));
-    alert.runModal();
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
